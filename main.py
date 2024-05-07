a69c37d4e9362b8d946337d004cbf5dd469e4db9
debajyoti-0923 <djdas2003@gmail.com>
2024-05-07 23:52:09 +0530

    added keepAlive

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ from dependencies import get_db
 import crud
 import asyncio
 from contextlib import asynccontextmanager
+import requests
+
+class keepAliveRender:
+    def __init__(self) -> None:
+        self.link=None
+    def createLink(self,link):
+        return f"https://{link}.onrender.com"
+    async def run_main(self):
+        while True:
+            if self.link is None:
+                await asyncio.sleep(5)
+                continue
+            response = requests.get(self.link)
+            await asyncio.sleep(840)
 
 class BackgroundRunner:
     async def run_main(self):
@@ -17,11 +31,13 @@ class BackgroundRunner:
             
 
 runner = BackgroundRunner()
+keepAliveRunner=keepAliveRender()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("starting background task")
     asyncio.create_task(runner.run_main())
+    asyncio.create_task(keepAliveRunner.run_main())
     yield
 
 app=FastAPI(lifespan=lifespan)
@@ -41,3 +57,11 @@ app.include_router(ingredients.router)
 app.include_router(pizza.router)
 app.include_router(orders.router)
 app.include_router(menu.router)
+
+@app.get("/")
+async def getRoot():
+    return {"message":"Hello from Debu's Pizza Service"}
+
+@app.post("/keepalive/{link}")
+async def keepAlive(link:str):
+    keepAliveRunner.link=keepAliveRunner.createLink(link)
